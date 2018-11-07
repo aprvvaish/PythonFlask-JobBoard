dc99f09d3cc0b960365f5a00bdae8259a9513f06
aprvvaish <apoorv.vaish007@gmail.com>
2018-11-07 09:59:11 +0530

    commit 2

--- jobs/app.py
+++ jobs/app.py
@@ -15,12 +15,15 @@ def open_connection():
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.excecute(sql, values)
-    if commit=True:
+
+    if commit == True:
         results = connection.commit()
     else:
         results = connection.fetchone() if single else fetchall()
+
     cursor.close()
     return results
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None )
